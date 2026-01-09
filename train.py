962bda70d4b5b3e3e91ae52d60964adad1d88b7d
Challa Sreeja <121494590+2022bcs0121-sreeja@users.noreply.github.com>
2026-01-09 12:41:42 +0530

    Model-Ridge, alpha-0.5, standardization

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import joblib
 import os
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error, r2_score
 
 # Ensure outputs directory exists
@@ -28,9 +28,13 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
+# Preprocessing
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
 # Model
-model = LinearRegression()
+model = Ridge(alpha=0.5)
 model.fit(X_train, y_train)
 
 # Prediction
